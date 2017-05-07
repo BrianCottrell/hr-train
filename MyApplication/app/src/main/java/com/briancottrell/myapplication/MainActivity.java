3b924dc8e4d447f079e86b98f87aa2269b0833f9
BrianCottrell <Brian_Cottrell@inbox.com>
2017-05-07 11:45:52 -0700

    Upload app

---
+++ MyApplication/app/src/main/java/com/briancottrell/myapplication/MainActivity.java
@@ -0,0 +1,58 @@
+package com.briancottrell.myapplication;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+
+        myWebView.getSettings().setJavaScriptEnabled(true);
+        myWebView.getSettings().setDomStorageEnabled(true);
+        myWebView.loadUrl("https://web.flock.co/");
+
+        myWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.action_video:
+
+            case R.id.action_audio:
+
+                return true;
+            case R.id.action_text:
+
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
